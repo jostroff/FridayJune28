9e02d74f08067873d1a577e4e8a3a712656cf492
185874 <185874@USREMWFHFV7P9H2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-06-28 13:37:11 -0500

    First commit.

---
+++ Documents/Workspace/EclipseOnNTT2/src/com/demo/StringDemo.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = "World";
+		if (s1.equals(s2))
+			System.out.println("String identical");
+		else
+			System.out.println("Strings not identical");
+		
+	}
+
+}
+/*Go to git, then open that git thing, then back to java, right click project, then team, then share,
+	then team again, and then commit.
+*/
